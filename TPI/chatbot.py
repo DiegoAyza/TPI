ffc054b77eb8b7e34c7539f5d06f3526da5d2bb6
diegoayza <diegoayza82@gmail.com>
2024-04-26 20:11:43 -0300

    Primer commit

---
+++ TPI/chatbot.py
@@ -0,0 +1,40 @@
+import csv
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import NearestNeighbors
+
+# Load data from CSV file
+# Function to load data from CSV file
+def load_data_from_csv(csv_file):
+    questions = []
+    answers = []
+    with open(csv_file, 'r', newline='') as file: 
+        reader = csv.reader(file, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        for row in reader:
+            questions.append(row[0])
+            answers.append(row[1])
+    return questions, answers
+
+# Load questions and answers from CSV
+questions, answers = load_data_from_csv('TPI/data.csv')
+
+# Vectorize the text data
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(questions)
+
+# Train a k-nearest neighbors model
+knn_model = NearestNeighbors(n_neighbors=1, algorithm='brute', metric='cosine')
+knn_model.fit(X)
+
+# Function to generate response
+def generate_response(input_text):
+    input_vector = vectorizer.transform([input_text])
+    _, index = knn_model.kneighbors(input_vector)
+    return answers[index[0][0]]
+
+# Chatbot interface
+def chatbot(respuesta):
+    response = generate_response(respuesta)
+    return response
+# Run the chatbot
+if __name__ == "__main__":
+    chatbot()
---
+++ TPI/loginHuella.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+
+# Base de datos de imágenes de huellas digitales (simulada)
+database = {
+    'usuario1': cv2.imread('TPI/img/huella1.jpg', cv2.IMREAD_GRAYSCALE),
+    'usuario2': cv2.imread('TPI/img/huella2.jpg', cv2.IMREAD_GRAYSCALE),
+    'usuario3': cv2.imread('TPI/img/huella3.jpg', cv2.IMREAD_GRAYSCALE)
+}
+
+def compare_fingerprints(fingerprint1, fingerprint2):
+    # Calculamos la diferencia absoluta entre las dos imágenes
+    difference = cv2.absdiff(fingerprint1, fingerprint2)
+    return np.sum(difference)
+
+def authenticate_fingerprint(input_fingerprint):
+    min_difference = 150000 #float('inf')
+    authenticated_user = None
+
+    for username, stored_fingerprint in database.items():
+        difference = compare_fingerprints(input_fingerprint, stored_fingerprint)
+        
+        if difference <= min_difference:
+            min_difference = difference
+            authenticated_user = username
+
+    return authenticated_user
+
+
+#input_fingerprint = cv2.imread('img/huella_a_comparar.jpg', cv2.IMREAD_GRAYSCALE)
+
+def decode_and_verify(image_file):
+    # Read the image file directly from memory
+    nparr = np.fromstring(image_file.read(), np.uint8)
+    input_fingerprint = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+
+    # Autenticación de la huella digital capturada
+    authenticated_user = authenticate_fingerprint(input_fingerprint)
+
+    return authenticated_user
+
+def auth(input):
+    try:
+        # Simulación de captura de huella digital (cargando una imagen)
+        input_fingerprint = cv2.imread(input, cv2.IMREAD_GRAYSCALE)
+        # Autenticación de la huella digital capturada
+        authenticated_user = authenticate_fingerprint(input_fingerprint)
+
+        if authenticated_user:
+            print(f'Autenticación exitosa. Bienvenido, {authenticated_user}.')
+        else:
+            print('Autenticación fallida. Usuario no reconocido.')
+    except:
+        print('direccion de imagen invalida.')
+
+def login():
+    print("\nHi! Please enter your fingerprint img path bellow. Type 'quit' to exit.")
+    while True:
+        user_input = input("Fingerprint path: ")
+        if user_input.lower() == 'quit':
+            print("ChatBot: Goodbye!")
+            break
+        auth(user_input)
+
+# Run the chatbot
+if __name__ == "__main__":
+    login()
